1bdbdccb0faa5db78dd7aeeee2efab609ffdf9e4
cjewell47 <charles.jewell47@gmail.com>
2018-10-28 22:28:16 +0000

    video 19, nesting templates

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+
+import Ninjas from './Ninjas.vue'
+
 export default {
+  components: {
+    'ninjas': Ninjas
+  },
   data () {
     return {
-      title: 'First Vue file'
-    }
-  },
-  methods: {
-    greeting: function() {
-      return 'What is up'
+      title: 'Ninja App'
     }
   }
 }
