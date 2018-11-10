5fe5f94b82518fc76bfe61f62c1837482bad01f5
cjewell47 <charles.jewell47@gmail.com>
2018-11-10 20:23:21 +0000

    30, blog, checkboxes

--- vuejs-blog/src/components/addBlog.vue
+++ vuejs-blog/src/components/addBlog.vue
@@ -6,23 +6,40 @@
       <input type="text" v-model.lazy="blog.title" required />
       <label>Blog Content:</label>
       <textarea v-model.lazy.trim="blog.content"></textarea>
+      <div id="checkboxes">
+        <p>Blog Categories:</p>
+        <label>Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories" />
+        <label>Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories" />
+        <label>Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories" />
+        <label>Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories" />
+      </div>
     </form>
     <div id="preview">
       <h3>Preview blog</h3>
-      <p>Blog title: {{ blog.title }}</p>
-      <p>Blog content:</p>
+      <p>Title: {{ blog.title }}</p>
+      <p>Content:</p>
       <p style="white-space: pre">{{ blog.content }}</p>
+      <p>Categories:</p>
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+// Imports
 export default {
   data () {
     return {
       blog: {
         title: '',
-        content: ''
+        content: '',
+        categories: []
       }
     }
   },
@@ -32,28 +49,35 @@ export default {
 </script>
 
 <style>
-  #add-blog * {
-    box-sizing: border-box;
-  }
-  #add-blog {
-    margin: 20px auto;
-    max-width: 500px;
-  }
-  label {
-    display: block;
-    margin: 20px 0 10px;
-  }
-  input[type="text"], textarea {
-    display: block;
-    width: 100%;
-    padding: 8px;
-  }
-  #preview {
-    padding: 10px 20px;
-    border: 1px dotted #ccc;
-    margin: 30px 0;
-  }
-  h3 {
-    margin-top: 10px;
-  }
+#add-blog * {
+  box-sizing: border-box;
+}
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
+}
+label {
+  display: block;
+  margin: 20px 0 10px;
+}
+input[type="text"], textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
+}
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+h3 {
+  margin-top: 10px;
+}
+#checkboxes input {
+  display: inline-block;
+  margin-right: 10px;
+}
+#checkboxes label{
+  display: inline-block;
+}
 </style>
